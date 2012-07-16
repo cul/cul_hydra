9d6f738aa61ffc799bfd2cd600dba8052e9c968b
Benjamin Armintor <armintor@gmail.com>
2012-07-16 17:51:44 -0400

    hyhead 4.1.1 compat

---
+++ app/models/solr_document.rb
@@ -0,0 +1,7 @@
+# -*- encoding : utf-8 -*-
+class SolrDocument
+
+  include Blacklight::Solr::Document
+
+  # this is a stub model for hydra-head-4.1.1 compatibility, and will be overridden by the output of the blacklight generator
+end
