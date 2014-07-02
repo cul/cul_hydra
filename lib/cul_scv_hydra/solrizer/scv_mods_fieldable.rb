de100424aa5f0468d10ba7a4de834717af298874
Eric O <eric@ejdigitalmedia.com>
2014-07-01 23:21:02 -0400

    Added lib_start_date_year_sim and lib_end_date_year_sim for holding only the year from start and end dates

--- lib/cul_scv_hydra/solrizer/scv_mods_fieldable.rb
+++ lib/cul_scv_hydra/solrizer/scv_mods_fieldable.rb
@@ -152,6 +152,17 @@ module Cul::Scv::Hydra::Solrizer
 			
 			solr_doc["lib_start_date_sim"] = start_date if start_date.present?
 			solr_doc["lib_end_date_sim"] = end_date if end_date.present?
+			
+			year_regex = /\A(\d\d\d\d).*/
+			
+			if start_date.present?
+				start_year_match = start_date[0].match(year_regex)
+				solr_doc["lib_start_date_year_sim"] = [start_date[0].match(year_regex).captures[0]] if start_year_match
+			end
+			if end_date.present?
+				end_year_match = end_date[0].match(year_regex)
+				solr_doc["lib_end_date_year_sim"] = [end_date[0].match(year_regex).captures[0]] if end_year_match
+			end
 
       solr_doc.each do |k, v|
         if self.class.maps_field? k
--- spec/unit/mods_document_spec.rb
+++ spec/unit/mods_document_spec.rb
@@ -306,7 +306,9 @@ ml
       solr_doc["origin_info_date_created_end_ssm"].should == ['1802']
       # check specially generated start_date and end_date fields
       solr_doc["lib_start_date_sim"].should == ['1801']
+      solr_doc["lib_start_date_year_sim"].should == ['1801']
       solr_doc["lib_end_date_sim"].should == ['1802']
+      solr_doc["lib_end_date_year_sim"].should == ['1802']
     end
     describe " date element handling" do
       it "handles date issued single" do
@@ -318,7 +320,9 @@ ml
         solr_doc["origin_info_date_issued_start_ssm"].should == nil
         solr_doc["origin_info_date_issued_end_ssm"].should == nil
         solr_doc["lib_start_date_sim"].should == ['1700']
+        solr_doc["lib_start_date_year_sim"].should == ['1700']
         solr_doc["lib_end_date_sim"].should == ['1700']
+        solr_doc["lib_end_date_year_sim"].should == ['1700']
       end
       it "handles date issued range" do
         item_xml = fixture( File.join("CUL_MODS", "mods-date-issued-range.xml") )
@@ -329,7 +333,9 @@ ml
         solr_doc["origin_info_date_issued_start_ssm"].should == ['1701']
         solr_doc["origin_info_date_issued_end_ssm"].should == ['1702']
         solr_doc["lib_start_date_sim"].should == ['1701']
+        solr_doc["lib_start_date_year_sim"].should == ['1701']
         solr_doc["lib_end_date_sim"].should == ['1702']
+        solr_doc["lib_end_date_year_sim"].should == ['1702']
       end
       it "handles date created single" do
         item_xml = fixture( File.join("CUL_MODS", "mods-date-created-single.xml") )
@@ -340,7 +346,9 @@ ml
         solr_doc["origin_info_date_created_start_ssm"].should == nil
         solr_doc["origin_info_date_created_end_ssm"].should == nil
         solr_doc["lib_start_date_sim"].should == ['1800']
+        solr_doc["lib_start_date_year_sim"].should == ['1800']
         solr_doc["lib_end_date_sim"].should == ['1800']
+        solr_doc["lib_end_date_year_sim"].should == ['1800']
       end
       it "handles date created range" do
         item_xml = fixture( File.join("CUL_MODS", "mods-date-created-range.xml") )
@@ -351,7 +359,9 @@ ml
         solr_doc["origin_info_date_created_start_ssm"].should == ['1801']
         solr_doc["origin_info_date_created_end_ssm"].should == ['1802']
         solr_doc["lib_start_date_sim"].should == ['1801']
+        solr_doc["lib_start_date_year_sim"].should == ['1801']
         solr_doc["lib_end_date_sim"].should == ['1802']
+        solr_doc["lib_end_date_year_sim"].should == ['1802']
       end
       it "handles date other single" do
         item_xml = fixture( File.join("CUL_MODS", "mods-date-other-single.xml") )
@@ -362,7 +372,9 @@ ml
         solr_doc["origin_info_date_other_start_ssm"].should == nil
         solr_doc["origin_info_date_other_end_ssm"].should == nil
         solr_doc["lib_start_date_sim"].should == ['1900']
+        solr_doc["lib_start_date_year_sim"].should == ['1900']
         solr_doc["lib_end_date_sim"].should == ['1900']
+        solr_doc["lib_end_date_year_sim"].should == ['1900']
       end
       it "handles date other range" do
         item_xml = fixture( File.join("CUL_MODS", "mods-date-other-range.xml") )
@@ -373,7 +385,9 @@ ml
         solr_doc["origin_info_date_other_start_ssm"].should == ['1901']
         solr_doc["origin_info_date_other_end_ssm"].should == ['1902']
         solr_doc["lib_start_date_sim"].should == ['1901']
+        solr_doc["lib_start_date_year_sim"].should == ['1901']
         solr_doc["lib_end_date_sim"].should == ['1902']
+        solr_doc["lib_end_date_year_sim"].should == ['1902']
       end
     end
   end
