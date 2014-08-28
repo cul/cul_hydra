75c42b309e401c1891292e5c970baceb669151f8
Benjamin Armintor <armintor@gmail.com>
2014-08-28 16:08:45 -0400

    json constant should be a string, not a bare word

--- app/models/generic_aggregator.rb
+++ app/models/generic_aggregator.rb
@@ -16,7 +16,7 @@ class GenericAggregator < ::ActiveFedora::Base
   def index_type_label
     riquery = Cul::Scv::Hydra::Models::MEMBER_ITQL.gsub(/%PID%/, self.pid)
     begin
-      docs = Cul::Scv::Fedora.repository.find_by_itql riquery, limit: 2, format: json
+      docs = Cul::Scv::Fedora.repository.find_by_itql riquery, limit: 2, format: 'json'
       docs = JSON.parse(docs)['results']
     rescue Exception=>e
       Rails.logger.warn("#{self.class.name} failed to find children with TQL: #{e.message}")
