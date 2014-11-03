896d5f0dda52558fb21b845f819953634022dc6b
Eric O <elo2112@columbia.edu>
2014-11-03 12:43:00 -0500

    Created Concept class; Updated Concept CModel xml

---
+++ app/models/concept.rb
@@ -0,0 +1,23 @@
+require "active-fedora"
+require "active_fedora_finders"
+class Concept < GenericAggregator
+  include ::ActiveFedora::FinderMethods::RepositoryMethods
+  include ::ActiveFedora::DatastreamCollections
+  include ::Hydra::ModelMethods
+  include Cul::Scv::Hydra::Models::Common
+
+  has_many :parts, :property => :cul_member_of, :class_name=>'ActiveFedora::Base'
+
+  def route_as
+    "concept"
+  end
+
+  def index_type_label
+    "CONCEPT"
+  end
+
+  def to_solr(solr_doc = Hash.new, opts={})
+    solr_doc = super(solr_doc, opts)
+    solr_doc
+  end
+end
