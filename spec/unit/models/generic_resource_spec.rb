d8372ddbea2b97459bc8a9d23a754581b3aa7789
Benjamin Armintor <armintor@gmail.com>
2015-03-02 18:22:02 -0500

    unit test fulltext and title text indexing on GenericResource

--- spec/unit/models/generic_resource_spec.rb
+++ spec/unit/models/generic_resource_spec.rb
@@ -12,5 +12,15 @@ describe GenericResource, type: :unit do
       doc = o.to_solr
       expect(doc['original_name_tesim']).to eql(['path to a thing'])
     end
+    it 'should index the original path as full text if present' do
+      o = GenericResource.new(pid:"lol:wut")
+      ft = double('Datastream')
+      o.datastreams['fulltext'] = ft
+      allow(ft).to receive(:content).and_return('foo')
+      allow(ft).to receive(:to_solr).and_return({})
+      fdoc = {"title_display_ssm" => ["Test title"]}
+      doc = o.to_solr(fdoc, model_only: true)
+      expect(doc['fulltext_tesim']).to eql(['Test title','foo'])
+    end
   end
 end
