dee3c1660d9dff6551220dae9686eaf4295a5cfb
Benjamin Armintor <armintor@gmail.com>
2015-08-31 12:41:23 -0400

    draft of pcdm AdministrativeSet

---
+++ app/models/administrative_set.rb
@@ -0,0 +1,68 @@
+require "active-fedora"
+require "active_fedora_finders"
+class AdministrativeSet < ::ActiveFedora::Base
+  include ::ActiveFedora::FinderMethods::RepositoryMethods
+  include ::ActiveFedora::DatastreamCollections
+  include Hydra::ModelMethods
+  include Cul::Hydra::Models::Common
+  include Pcdm::Models
+
+  has_and_belongs_to_many :members, :property => :ldp_contains, :class_name=>'ActiveFedora::Base'
+
+  rdf_types(RDF::CUL.Aggregator)
+  rdf_types(RDF::PCDM.AdministrativeSet)
+
+  def route_as
+    "administrative_set"
+  end
+
+  def has_struct_metadata?
+    false
+  end
+
+  def solr_members(opts={})
+    opts = {:rows=>25,:response_format=>:solr}.merge(opts)
+    r = self.parts(opts)
+    members = []
+    r.collect {|hit| members << SolrDocument.new(hit) } unless r.blank?
+    members
+  end
+
+  def thumbnail_info
+    members = solr_members
+    if members.length == 0
+      thumb = {:asset=>"cul_hydra/crystal/file.png",:mime=>'image/png'}
+    else
+      thumb = thumb_from_members(members)
+    end
+    return thumb || {:asset=>"cul_hydra/crystal/file.png",:mime=>'image/png'}
+  end
+
+  private
+  def thumb_from_members(members)
+    sorted = members.sort do |a,b|
+      c = a['title_si'] <=> b['title_si']
+      if c == 0 && a['identifier_ssim']
+        if b['identifier_ssim']
+          a['identifier_ssim'].delete(a.id) unless a['identifier_ssim'].length == 1
+          b['identifier_ssim'].delete(a.id) unless b['identifier_ssim'].length == 1
+          a['identifier_ssim'][0] <=> b['identifier_ssim'][0]
+        else
+          -1
+        end
+      else
+        c
+      end
+    end
+    thumb_from_solr_doc(sorted[0])
+  end
+
+  def thumb_from_solr_doc(solr_doc)
+    if solr_doc and (member =  ActiveFedora::Base.find(solr_doc.id, :cast=>true)).respond_to? :thumbnail_info
+      member.thumbnail_info
+    else
+      return nil
+    end
+  end
+
+end
---
+++ spec/unit/models/administrative_set_spec.rb
@@ -0,0 +1,40 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe AdministrativeSet, type: :unit do
+
+  before(:all) do
+
+  end
+  describe '.to_class_uri' do
+    let(:to_class_uri) { AdministrativeSet.to_class_uri }
+    subject { to_class_uri }
+    it do
+      is_expected.to eql("info:fedora/pcdm:AdministrativeSet")
+    end
+    context "is used as the value to look up the name of the model class" do
+      subject { ActiveFedora::Model.classname_from_uri(to_class_uri) }
+      it do
+        is_expected.to eql ['AdministrativeSet', 'pcdm']
+      end
+    end
+    context "is used as the value to look up the model class" do
+      subject { ActiveFedora::ContentModel.uri_to_model_class(to_class_uri) }
+      it do
+        is_expected.to be AdministrativeSet
+      end
+    end
+  end
+  describe '#rdf_types!' do
+    let(:aggregator) {
+      agg = AdministrativeSet.new
+      allow(agg).to receive(:save).and_return(agg)
+      agg
+    }
+    subject { aggregator.rdf_types! }
+    it do
+      expect(subject.relationships(:rdf_type)).to include(RDF::CUL.Aggregator.to_s)
+      expect(subject.relationships(:rdf_type)).to include(RDF::PCDM.AdministrativeSet.to_s)
+      expect(subject.relationships(:rdf_type)).not_to include(RDF::PCDM.Object.to_s)
+    end
+  end
+end
