faa6d2203e6d528d03242e5372d12ff94db117e2
Benjamin Armintor <armintor@gmail.com>
2019-06-16 14:58:42 -0400

    v1.7.1 accessControlMetadata with has_metadata for indexing

--- spec/unit/models/generic_resource_spec.rb
+++ spec/unit/models/generic_resource_spec.rb
@@ -39,6 +39,27 @@ describe GenericResource, type: :unit do
     it do
       expect(o.to_solr['datastreams_ssim']).to eql(datastream_ids)
     end
+    context "it has accessControlMetadata" do
+      let(:ds_content) { fixture(File.join("CUL_ACCESS", "access-conditions.xml")) }
+      let(:solr_doc) { o.to_solr }
+      before { o.datastreams['accessControlMetadata'].content = ds_content }
+
+      it "has permitted affils" do
+        expect(solr_doc['access_control_affiliations_ssim']).to eql(['LIB_role-ext-UnivSemArchives'])
+      end
+      it "has permitted locations" do
+        expect(solr_doc['access_control_locations_ssim']).to eql(['http://id.library.columbia.edu/term/45487bbd-97ef-44b4-9468-dda47594bc60'])
+      end
+      it "has permitted date" do
+        expect(solr_doc['access_control_embargo_dtsi']).to eql('2099-01-01')
+      end
+      it "has permissions flag" do
+        expect(solr_doc['access_control_permissions_bsi']).to eql(true)
+      end
+      it "has access levels" do
+        expect(solr_doc['access_control_levels_ssim'].sort).to eql(['Embargoed','On-site Access','Specified Group/UNI Access'])
+      end
+    end
   end
 
   context 'image rotation' do
