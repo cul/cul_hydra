e7d0304cbd79b1d0cf35337d3be9890da3ad7c4a
Benjamin Armintor <armintor@gmail.com>
2020-03-06 23:23:01 -0500

    remove unused controller mixins

--- lib/cul_hydra/controllers/helpers/dc_metadata_helper_behavior.rb
+++
@@ -1,9 +0,0 @@
-module Cul::Hydra::Controllers::Helpers
-  module DcMetadataHelperBehavior
-    def dcmi_types
-      ['', 'Collection', 'Dataset', 'Event', 'Image', 'InteractiveResource',
-       'MovingImage', 'PhysicalObject', 'Service', 'Software', 'Sound',
-       'StillImage', 'Text']
-    end
-  end
-end
