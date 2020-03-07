0294862c6778c99b9c4ca890081cb102f47e05e6
Benjamin Armintor <armintor@gmail.com>
2020-03-06 23:24:20 -0500

    remove unused controller mixins

--- lib/cul_hydra.rb
+++ lib/cul_hydra.rb
@@ -1,10 +1,8 @@
-require 'hydra/head'
 require 'active_fedora_relsint'
 require 'cul_hydra/exceptions'
 require 'cul_hydra/solrizer_patch'
 module Cul
   module Hydra
-    autoload :Controllers, 'cul_hydra/controllers'
     autoload :Fedora, 'cul_hydra/fedora'
     autoload :Om, 'cul_hydra/om'
     autoload :Solrizer, 'cul_hydra/solrizer'
@@ -13,7 +11,6 @@ module Cul
 end
 
 require "cul_hydra/access_controls_enforcement"
-require "cul_hydra/controllers"
 require "cul_hydra/om"
 require "cul_hydra/indexer"
 require "cul_hydra/risearch_members"
